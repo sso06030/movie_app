254574414379e41660247594aaa8a2362cc29425
kim-yeji <sso06030@khu.ac.kr>
2020-07-21 13:16:34 +0900

    Updates

---
+++ precache-manifest.47ec3db251211e7429ae374d5d0b2bc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9cde4e5650487e9590399c1085f4f1",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "09f4317b941ca51640df",
+    "url": "/movie_app/static/css/main.0b0a1b33.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09f4317b941ca51640df",
+    "url": "/movie_app/static/js/main.da5d12de.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
